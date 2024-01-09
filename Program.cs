2e3dca62268d36b9205b08283317f7cd4eda2235
Edvá <edvalima25gmail.com>
2024-01-09 15:10:59 -0300

    Resolução do Projeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
-﻿using DesafioPOO.Models;
+﻿﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("863526", "tipo 5", "456828742", 112);
+nokia.Ligar();
+nokia.InstalarAplicativo("DIO");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone("256366", "11", "458269", 224);
+iphone.Ligar();
+iphone.InstalarAplicativo("Chorme");
